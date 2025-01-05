772a3219f78ec5537ae93c877dae646fe964bd38
jnrslm3 <jumabekovn43@gmail.com>
2025-01-05 15:53:22 +0600

    _

--- commands/command.py
+++ commands/command.py
@@ -12,3 +12,11 @@ async def start_handler(message: Message):
     await message.answer("You started the bot!")
 
 
+@command_router.message(F.text)
+async def delete_keyword_messages(message: Message):
+    for keyword in KEYWORDS:
+        if keyword in message.text.lower():
+            try:
+                await message.delete()
+                logging.info(f"Deleted message: {message.text}")
+            
