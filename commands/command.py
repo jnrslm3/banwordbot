763fd18826cfa50767106cf2af33ff6ce58fabd5
jnrslm3 <jumabekovn43@gmail.com>
2025-01-06 18:38:45 +0600

    added database

--- commands/command.py
+++ commands/command.py
@@ -3,32 +3,30 @@ from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery, FSInputFile
 import logging
 from commands.keyboards import *
+import re
 
 command_router = Router()
 
-def keywords(file_path: "/Users/nurislam/Desktop/projects/telegram_bots/ban_word_bot/list_of_badwords.txt"):
-    with open(file_path, 'r') as file:
-        return [line.strip().lower() for line in file]
+# def keywords(file_path: "/Users/nurislam/Desktop/projects/telegram_bots/ban_word_bot/list_of_badwords.txt"):
+#     with open(file_path, 'r') as file:
+#         return [line.strip().lower() for line in file]
 
-KEYWORDS = keywords("list_of_badwords.txt")
+# KEYWORDS = keywords("list_of_badwords.txt")
 
 @command_router.message(Command("start"))
 async def start_handler(message: Message):
-    await message.answer("""Вы активировали бота. Этот бот удаляет все матершинные слова в чате группы или канала. \n 
-    Если хотите добавить бота к себе в группу следуйте по инструкции""" , reply_markup = await inline_buttons())
+    await message.answer("""Добро пожаловать! Вы успешно активировали бота. Этот бот автоматически удаляет матершинные слова из сообщений в чате группы или канала. 
 
+        Если вы хотите добавить бота в свою группу, пожалуйста, следуйте инструкции ниже.""" , reply_markup = await inline_buttons())
 
-@command_router.message(F.text)
-async def delete_keyword_messages(message: Message):
-    for keyword in KEYWORDS:
-        if keyword in message.text.lower():
-            await message.delete()
+# @command_router.message(F.text)
+# async def delete_keyword_messages(message: Message):
+#     for keyword in KEYWORDS:
+        
 
 @command_router.callback_query(F.data.startswith('help'))
 async def help_handler(callback: CallbackQuery):
     await callback.message.answer("Для дополнительной помощи обращайтесь сюда @jnrslm3 ")
-
-from aiogram.types import InputFile
 
 @command_router.callback_query(F.data.startswith('guide'))
 async def guide_handler(callback: CallbackQuery):
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import sys, logging
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from commands.command import command_router
+from databases.models import *
 
 async def main():
-    bot = Bot(token=TOKEN)
-    dp = Dispatcher()
-    dp.include_router(command_router)
-    await dp.start_polling(bot)
+    # bot = Bot(token=TOKEN)
+    # dp = Dispatcher()
+    # dp.include_router(command_router)
+    # await dp.start_polling(bot)
+    await create_tables()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
